3f1cbd74f067d0813c53055eb8b7f87ed7b557ab
kayne <kayne.steele404@gmail.com>
2019-01-16 23:27:50 -0600

    modified:   configSync.sh

--- configSync.sh
+++ configSync.sh
@@ -1,3 +1,11 @@
+sudo cp ~/test/sources.list /etc/apt/sources.list 
+
+cp -r ~/test/wallpapers ~/
+cat ~/test/status.sh > ~/status.sh
+cat ~/test/Xdefaults > ~/.Xdefaults
+cat ~/test/xinitrc > ~/.xinitrc
+cat ~/test/bash_aliases > ~/.bash_aliases
+
 if [[ ! $(command -v vim) ]]; then
     sudo apt install vim
 fi
@@ -6,11 +14,5 @@ if [[ ! $(command -v ranger) ]]; then
     sudo apt install ranger
 fi
 
-cp -r ~/test/wallpapers ~/wallpapers
-cat ~/test/status.sh > ~/status.sh
-cat ~/test/Xdefaults > ~/.Xdefaults
-cat ~/test/xinitrc > ~/.xinitrc
-cat ~/test/bash_aliases > ~/.bash_aliases
 sudo cp ~/test/vimrc /etc/vim/vimrc
 sudo cp ~/test/rc.conf /etc/ranger/config/rc.conf
-sudo cp ~/test/sources.list /etc/apt/sources.list 
