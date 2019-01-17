079c690649d345802b1e0d75d4d8c2591e0bcd36
kayne <kayne.steele404@gmail.com>
2019-01-16 23:02:01 -0600

    modified:   status.sh

--- status.sh
+++ status.sh
@@ -1,11 +1,11 @@
 while true;
-    dateVar=$(date +"%m/%d/%Y")
-    bat=$(cat /sys/class/power_supply/BAT0/capacity)
-    batStatus=$(cat /sys/class/power_supply/BAT0/status)
-    time=$(date +'%r')
-    diskUsage=$(df -h / | awk '{print $4}' | grep -e "[0-9]\{1,\}G")
-    #ip=$(ifconfig |  grep -A 1 'wlp3s0' | tail -1 | cut -d ':' -f 2 | awk '{print $2}') 
-    #ip1=$(ifconfig |  grep -A 1 'enp0s25' | tail -1 | cut -d ':' -f 2 | awk '{print $2}') 
-do
-        xsetroot -name "$dateVar | $time | $bat% | $diskUsage"
-done
+dateVar=$(date +"%m/%d/%Y")
+bat=$(cat /sys/class/power_supply/BAT0/capacity)
+batStatus=$(cat /sys/class/power_supply/BAT0/status)
+time=$(date +'%r')
+diskUsage=$(df -h / | awk '{print $4}' | grep -e "[0-9]\{1,\}G")
+ip=$(ip route get 1.2.3.4 | awk '{print $7}') 
+    do
+        xsetroot -name "$dateVar | $time | $bat% | $diskUsage | $ip"
+        sleep 1
+    done
